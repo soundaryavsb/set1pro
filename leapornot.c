b4df0aff87252327074011429e0ac8b44126142b
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-01-18 10:17:07 +0000

    Create leapornot.c

---
+++ leapornot.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("Even");
+	}
+	else
+	{
+		printf("Odd");
+	}
+	
+	// your code goes here
+	return 0;
+}
